25dc6a9de8326629c2488ca7c48e01ffd03474db
joje <joje.attrs@gmail.com>
2016-10-14 23:05:59 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# ng-dnd
+> An angularjs directives for html5 drag and drop events.
+
+## Installation
+```sh
+$ npm install ng-dnd --save
+```
+
---
+++ ng-mediumeditor.js
@@ -0,0 +1,92 @@
+var angular = require('angular');
+var MediumEditor = require('medium-editor');
+require('medium-editor/dist/css/medium-editor.css');
+require('medium-editor/dist/css/themes/default.css');
+
+
+function directive($compile) {
+  function toInnerText(value) {
+    var tempEl = document.createElement('div'), text;
+    tempEl.innerHTML = value;
+    text = tempEl.textContent || '';
+    return text.trim();
+  }
+  
+  return {
+    require: '?ngModel',
+    restrict: 'AE',
+    replace: true,
+    scope: { bindOptions: '=' },
+    template: '<div class="editable"></div>',
+    link: function(scope, element, attrs, ngModel) {
+      var defaultContentSrc = attrs['defaultContentSrc'];
+      
+      var editor = new MediumEditor(element, {
+        placeholder: false,
+        toolbar: {
+          allowMultiParagraphSelection: true,
+          buttons: ['bold', 'italic', 'underline', 'anchor', 'h1', 'h2', 'h3', 'quote'],
+          diffLeft: 0,
+          diffTop: -10,
+          firstButtonClass: 'medium-editor-button-first',
+          lastButtonClass: 'medium-editor-button-last',
+          relativeContainer: null,
+          standardizeSelectionStart: false,
+          static: false,
+          align: 'center',
+          sticky: false,
+          updateOnEmptySelection: false
+        },
+        anchor: {
+          customClassOption: null,
+          customClassOptionText: 'Button',
+          linkValidation: true,
+          placeholderText: '링크를 입력하세요',
+          targetCheckbox: true,
+          targetCheckboxText: '새창으로 열기'
+        },
+        paste: {
+          forcePlainText: true,
+          cleanPastedHTML: false,
+          cleanReplacements: [],
+          cleanAttrs: ['class', 'style', 'dir'],
+          cleanTags: ['meta'],
+          unwrapTags: []
+        }
+      });
+      
+      ngModel.$render = function() {
+        element.html(ngModel.$viewValue || '');
+        var placeholder = editor.getExtensionByName('placeholder');
+        if (placeholder) {
+          placeholder.updatePlaceholder(element[0]);
+        }
+      };
+      
+      ngModel.$isEmpty = function(value) {
+        if (/[<>]/.test(value)) {
+          return toInnerText(value).length === 0;
+        } else if (value) {
+          return value.length === 0;
+        } else {
+          return true;
+        }
+      };
+      
+      editor.subscribe('editableInput', function(event, editable) {
+        ngModel.$setViewValue(editable.innerHTML.trim());
+      });
+      
+      scope.$watch('options', function(bindOptions) {
+        editor.init(element, bindOptions);
+      });
+      
+      scope.$on('$destroy', function() {
+        editor.destroy();
+      });
+    }
+  };
+}
+
+angular.module('ngMediumEditor', []).directive('mediumEditor', directive);
+module.exports = directive;
