2c2b2991f480c4ece73da9617064b62db17b0780
joje <joje.attrs@gmail.com>
2016-10-15 21:11:11 +0900

    ..

--- ng-mediumeditor.js
+++ ng-mediumeditor.js
@@ -3,7 +3,6 @@ var MediumEditor = require('medium-editor');
 require('medium-editor/dist/css/medium-editor.css');
 require('medium-editor/dist/css/themes/default.css');
 
-
 function directive($compile) {
   function toInnerText(value) {
     var tempEl = document.createElement('div'), text;
